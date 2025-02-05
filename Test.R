d745904db33eaded95047d6c07d49a58481c40f9
Nicole Ward <Nicole.Ward@state.mn.us>
2025-02-05 13:02:58 -0600

    loaad and manipulate fish data. initial plot.

--- Test.R
+++ Test.R
@@ -1,7 +1,9 @@
 
+# see R tidy text book: https://r4ds.hadley.nz/whole-game
+
 #### install packages ####
 # install.packages("tidyverse") # run this line if you need to install tidyverse
-install.packages("styler") # run this line if you need to install styler
+# install.packages("styler") # run this line if you need to install styler
 
 #### Load Packages ####
 library(tidyverse) # load tidyverse package
@@ -11,8 +13,24 @@ library(ggthemes) # contains colorblind friendly color palettes
 fishdata <- read_csv("ltrm_fish_data.csv") # Read In Fish Data
 glimpse(fishdata) # take a peek at the data
 
-#### Tidy Data ####
+#### Organize Data ####
+# create data table of just lentic fish, as listed in Status and Trends 2022
+lentic <- filter(fishdata,fishcode=="LMBS"|fishcode == "BLGL"|fishcode=="BWFN"|
+                   fishcode=="WDSN"|fishcode=="NTPK"|fishcode=="BKCP"|
+                   fishcode=="LNGR"|fishcode=="WTCP"|fishcode=="YWPH"|
+                   fishcode=="GDSN"|fishcode=="STGR"|fishcode=="GNSF"|
+                   fishcode=="PNSD"|fishcode=="OSSF")
+
+# create table of annual total lentic fish catch by strata
+lentic$sdate<-mdy(lentic$sdate) #convert to date column type
+lentic_annualsum <- lentic %>% 
+  mutate(year = year(sdate)) %>%  #create year column
+  group_by(year,stratum) %>%      # group by year and strata
+  summarise(TotCatch = sum(catch))# calculate total catch per strata each year
+  
 
 #### Visualize Data ####
-ggplot(data = fishdata,
-       mapping = aes(x=sdate,))
+ggplot(data = lentic_annualsum,
+       mapping = aes(x=year,y=TotCatch,color = stratum))+
+  geom_point()+
+  geom_smooth()
